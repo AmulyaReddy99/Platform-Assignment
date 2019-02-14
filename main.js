344bcb8d3229b0c2378ad27bb0f2d5cda7b8bf84
Amulya Reddy <amulyareddyk97@gmail.com>
2019-02-15 00:56:50 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onclick = function(event) {
+    if (event.target == (modal = document.getElementById('abstract'))) {
+        modal.style.display = "none";
+    }
+    else if (event.target == (modal = document.getElementById('algorithm'))){
+      modal.style.display = "none";
+    }
+    else if (event.target == (modal = document.getElementById('use'))){
+    modal.style.display = "none";
+    }
+    else if (event.target == (modal = document.getElementById('tools'))){
+      modal.style.display = "none";
+    }
+} 
